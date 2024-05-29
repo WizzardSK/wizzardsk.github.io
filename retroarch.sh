221809060ac502aba7bad7ef8eb271142bb295fb
Wizzard <wizzardsk@gmail.com>
2024-05-29 22:37:57 +0200

    c128

--- retroarch.sh
+++ retroarch.sh
@@ -151,6 +151,7 @@ case "$adresar" in
 *"Commodore 64 Sports") core="vice_x64sc_libretro";;
 *"Commodore 64 Strategy") core="vice_x64sc_libretro";;
 *"Commodore 64 Educational") core="vice_x64sc_libretro";;
+*"Commodore 128 D64") core="vice_x128_libretro";;
 *"Commodore - Amiga") core="puae_libretro";;
 *"Amiga 1200 ADF") core="puae_libretro";;
 *"Amiga 1200 SPS") core="puae_libretro";;
