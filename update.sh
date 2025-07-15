144e5a733a89957fd147e1eaa5e372113c5a2b4d
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-15 08:54:09 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/gameflix.zip
+unzip -o gameflix.zip
+git config --global user.name "GitHub Actions"
+git config --global user.email "actions@github.com"
+git add .
+git commit -m "Auto update ($(date +'%Y-%m-%d %H:%M:%S'))"
+git push
