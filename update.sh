f4ad8064ddc072860ddb6599346d4cfaceb355b9
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-15 08:57:12 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/gameflix.zip
 unzip -o gameflix.zip
+rm gameflix.zip
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 git add .
