25beb14b5a5f1d9cc8395db2686abdb6911593ea
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-18 14:19:28 +0200

    Update platform.js

--- platform.js
+++ platform.js
@@ -79,11 +79,24 @@ function generateUzeLinks(romPath, imagePath) {
 }
 
 function generateFileLinks(romPath, imagePath) {
-    document.write("<div id=\"figureList\">"); fileNames.forEach(fileName => {
+    //document.write("<div id=\"figureList\">"); fileNames.forEach(fileName => {
+    //    const subor = fileName.includes("\t") ? fileName.split("\t")[0] : fileName;
+    //    const nameWithoutExt = subor.slice(0, subor.lastIndexOf(".")) || subor;
+    //    const nazov = fileName.includes("\t") ? fileName.split("\t")[1] : fileName.replace(/\.[^.]+$/, "");
+    //    document.write(`<a href="../${romPath}/${encodeURIComponent(subor)}" target="main">
+    //    <figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/${imagePath}/master/Named_Snaps/${encodeURIComponent(nameWithoutExt)}.png" alt="${nameWithoutExt}"><figcaption>${nazov}</figcaption></figure></a>`);
+    //}); document.write("</div>");
+    
+    document.write("<div id=\"figureList\">");
+    const baseRomURL = location.protocol === "file:" ? "../" : "https://myrient.erista.me/files/";
+    fileNames.forEach(fileName => {
         const subor = fileName.includes("\t") ? fileName.split("\t")[0] : fileName;
         const nameWithoutExt = subor.slice(0, subor.lastIndexOf(".")) || subor;
         const nazov = fileName.includes("\t") ? fileName.split("\t")[1] : fileName.replace(/\.[^.]+$/, "");
-        document.write(`<a href="../${romPath}/${encodeURIComponent(subor)}" target="main">
+        document.write(`<a href="${baseRomURL}${romPath}/${encodeURIComponent(subor)}" target="main">
         <figure><img loading="lazy" src="https://raw.githubusercontent.com/WizzardSK/${imagePath}/master/Named_Snaps/${encodeURIComponent(nameWithoutExt)}.png" alt="${nameWithoutExt}"><figcaption>${nazov}</figcaption></figure></a>`);
-    }); document.write("</div>");
+    });
+    document.write("</div>");
 }
+
+
