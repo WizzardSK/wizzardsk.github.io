79abc79a1f700954893a84bb5461b76e1cfb4948
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-07-26 08:24:12 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/gameflix.zip
+wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/lowresnx.zip
+wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/tic80.zip
+wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/uzebox.zip
+wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/wasm4.zip
 unzip -o gameflix.zip
 rm gameflix.zip
 git config --global user.name "GitHub Actions"
