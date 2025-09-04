d6c773f74d005d7f136e49ac5b001448a1e3cefd
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-09-04 21:37:22 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,16 @@
-wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/gameflix.zip
 wget -nv -O lowresnx.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/lowresnx.zip
 wget -nv -O tic80.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/tic80.zip
 wget -nv -O uzebox.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/uzebox.zip
 wget -nv -O wasm4.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/wasm4.zip
+
+wget -nv -O atari2600.zip https://github.com/WizzardSK/Atari_-_2600/raw/refs/heads/main/indexes.zip
+unzip -q -o atari2600.zip -d atari2600
+rm atari2600.zip
+
+wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/gameflix.zip
 unzip -q -o gameflix.zip
 rm gameflix.zip
+
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
 git add .
