f250be06f3300caae3d9379e39021b8b70111859
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-09-05 08:45:40 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,11 +2,6 @@ wget -nv -O lowresnx.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/ma
 wget -nv -O tic80.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/tic80.zip
 wget -nv -O uzebox.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/uzebox.zip
 wget -nv -O wasm4.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/wasm4.zip
-
-wget -nv -O atari2600.zip https://github.com/WizzardSK/Atari_-_2600/raw/refs/heads/master/indexes.zip
-unzip -q -o atari2600.zip -d atari2600
-rm atari2600.zip
-
 wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/gameflix.zip
 unzip -q -o gameflix.zip
 rm gameflix.zip
