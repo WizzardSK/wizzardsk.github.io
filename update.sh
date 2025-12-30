55f16f55dfff7fb405a6a763676ddc301e672708
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2025-12-30 20:16:52 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 wget -nv -O lowresnx.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/lowresnx.zip
-wget -nv -O tic80.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/tic80.zip
 wget -nv -O wasm4.zip https://github.com/WizzardSK/gameflix/raw/refs/heads/main/fantasy/wasm4.zip
 wget -nv https://github.com/WizzardSK/gameflix/raw/refs/heads/main/gameflix.zip
 unzip -q -o gameflix.zip
